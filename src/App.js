c8bd163a8e75b61b13d68ad28ee4bcd740304273
caleb-ola <olajiire2@gmail.com>
2022-07-25 12:32:02 +0100

    Feat: Added header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Routes } from 'react-router-dom';
 // import Hatspage from './pages/hatspage/hatspage.component';
 import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         {/* <Route path="/hats" element={<Hatspage/>}/> */}
